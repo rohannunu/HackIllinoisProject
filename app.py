ce2cda668aac3a055a8134c21fbfdc31cf0746f2
georgehuber <georgehuber8@gmail.com>
2023-02-25 02:24:29 -0600

    initial country projection support

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, request, jsonify, render_template
-import time
+from flask import Flask, request,  render_template
+import json
+import pred_country
 app = Flask(__name__)
 
 
-parameterList = ["population", "pollution", "nature", "GDP"]
+parameterList = ["Ladder", "Positive affect","Social support", "Freedom", "Corruption", "Generosity", "Log of GDP per capita", "Healthy life expectancy", "CO2 Emissions"]
 
 @app.route('/')
 def index():
     return render_template("index.html", parameterList=parameterList, parameterListLen=len(parameterList))
 
-
+@app.route('/getCountry', methods = ["POST","GET"])
+def getCountry():
+    model_input = []
+    data =json.loads( request.form.get("parameters"))
+    for param in parameterList:
+        arg = data[param]
+        model_input.append(int(arg) if arg else 0)
+    country = pred_country.pred_country(model_input)
+    return country
+                                            
+       
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=81)
+    app.run(host='0.0.0.0', port=81, debug=True) #TODO: change
 
 
     
--- pred_country.py
+++ pred_country.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn import preprocessing
 
+
 df = pd.read_csv("out.csv")
 X = df[["Ladder", "Positive affect","Social support", "Freedom", "Corruption", "Generosity", "Log of GDP per capita", "Healthy life expectancy", "CO2 Emissions"]]
 y  = df['Country']
@@ -12,8 +13,20 @@ X_scaled = scaler.transform(X)
 
 clf = RandomForestClassifier(max_depth = 6)
 clf.fit(X_scaled, y)
-input = [[9,18.0,20.0,9.0,11.0,14.0,19.0,8.0,0.069134]]
-inp_scaled = scaler.transform(input)
+if __name__ == "__main__":
+    main()
 
-print(clf.predict(inp_scaled))
-print(clf.predict_proba(inp_scaled))
+
+
+
+def pred_country(model_input):
+    inp_scaled = scaler.transform([model_input])
+    res = clf.predict(inp_scaled)
+    return res[0]
+
+def main():
+    input = [[9,18.0,20.0,9.0,11.0,14.0,19.0,8.0,0.069134]]
+    inp_scaled = scaler.transform(input)
+    print(clf.predict(inp_scaled))
+    print(clf.predict_proba(inp_scaled))
+
