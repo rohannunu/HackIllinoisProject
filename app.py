b6d8abfab4c1ed3d36548e16b07d7fbd1b7bb4d4
Rohan Nunugonda <45941834+rohannunu@users.noreply.github.com>
2023-02-26 01:50:27 -0600

    yes

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, request,  render_template, jsonify
 import json
 import pred_country
-import points as points 
 app = Flask(__name__)
 
 
 parameterList = ["Ladder", "Positive affect","Social support", "Freedom", "Corruption", "Generosity", "Log of GDP per capita", "Healthy life expectancy", "CO2 Emissions", "Population", "Yearly Change", "Density", "Land Area", "Urban Pop %"]
-newPointsValue = points.getPointCount()
+totalAvailablePoints = 750
 
 @app.route('/')
 def index():
-    return render_template("index.html", parameterList=parameterList, parameterListLen=len(parameterList), userPointCount = newPointsValue)
+    return render_template("index.html", parameterList=parameterList, parameterListLen=len(parameterList))
 
 @app.route('/getCountry', methods = ["POST","GET"])
 def getCountry():
@@ -23,11 +22,10 @@ def getCountry():
     return country
 
 
-@app.route('/updatePointValue', methods=["POST"])
-def updatePointValue():
-    newPointsValue = points.getPointCount()
-    return jsonify('', render_template('getCountModel.html', userPointCount = newPointsValue))
-                                            
+@app.route('/updatePointValue/', methods=["POST"])
+def updateInternalPointValue():
+    sumOfSliders = json.loads( request.form.get("totalVal"))
+    return str(totalAvailablePoints - sumOfSliders)
        
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81, debug=False) #TODO: change
--- static/main.js
+++ static/main.js
@@ -13,3 +13,21 @@ function changeCountry(){
         document.getElementById("my-country-header").innerHTML = "Your results are most similar to "+data+".";
       });
 }
+
+function sendValue(){
+    totalVal = 0
+    for (i in parameterList){
+        param = parameterList[i]
+        let val = document.getElementById(param).value
+        totalVal += parseInt(val)
+    }
+    if (totalVal > 750) {
+        totalVal = 750
+    }
+    $.post("/updatePointValue/",
+    {"totalVal":JSON.stringify(totalVal)},
+    function(data, status){ //This gets back whatever Flask function calls
+        console.log(data)
+        document.getElementById("pointVal").innerHTML = data
+      });
+}
