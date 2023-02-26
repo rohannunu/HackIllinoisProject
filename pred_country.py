509180f6bda5d4be022ac2c71f7bf7e46942546a
Bill Zhang <billjzhang@yahoo.com>
2023-02-25 19:36:10 -0600

    changed ML

--- pred_country.py
+++ pred_country.py
@@ -8,11 +8,10 @@ df = pd.read_csv("out.csv")
 X = df[["Ladder", "Positive affect","Social support", "Freedom", "Corruption", "Generosity", "Log of GDP per capita", "Healthy life expectancy", "CO2 Emissions", "Population", "Yearly Change", "Density", "Land Area", "Urban Pop %"]]
 y  = df['Country']
 
-scaler = preprocessing.StandardScaler().fit(X)
-
+scaler = preprocessing.MinMaxScaler(feature_range=(0,1)).fit(X)
 X_scaled = scaler.transform(X)
 
-clf = DecisionTreeClassifier(max_depth = 4)
+clf = RandomForestClassifier(max_depth = 4)
 clf.fit(X_scaled, y)
 
 input = [[9,18.0,20.0,9.0,11.0,14.0,19.0,8.0,0.069134,5542237,0.0015,18,303890,0.86]]
@@ -20,7 +19,11 @@ inp_scaled = scaler.transform(input)
 #print(clf.predict(inp_scaled))
 #print(clf.predict_proba(inp_scaled))
 
-
+#Testing
+X_s = df[['Density']]
+scaler2 = preprocessing.MinMaxScaler(feature_range=(0,1)).fit(X_s)
+X_scaled2 = scaler2.transform(X_s)
+print(X_scaled2)
 
 # Need to move this somewhere else, but will keep this for now
 def main():
@@ -31,9 +34,12 @@ def main():
 
 def pred_country(model_input):
     #"Ladder", "Positive affect","Social support", "Freedom", "Corruption", "Generosity", "Log of GDP per capita", "Healthy life expectancy", "CO2 Emissions", "Population", "Yearly Change", "Density", "Land Area", "Urban Pop %"
-    scaler = preprocessing.StandardScaler().fit([model_input])
-    inp_scaled = scaler.transform([model_input])
-    res = clf.predict(inp_scaled)
+    model_input = pd.Series(model_input)
+    model_input = model_input/100
+    #inp_scaled = scaler.transform([model_input])
+    res = clf.predict([model_input])
+    print(X_scaled)
+    print(model_input)
     print(res)
     return res[0]
 
